075eab308bb413d9d2496969e3e20b2c6e2d7572
Guadalupemora <tereliye.pumita@gmail.com>
2022-06-16 00:36:03 -0500

    desafio4

--- src/components/NarBar/NavBar.js
+++ src/components/NarBar/NavBar.js
@@ -1,5 +1,3 @@
-
-
 import { Link, NavLink } from "react-router-dom"
 
 const NavBar = () => {
@@ -13,9 +11,7 @@ const NavBar = () => {
            
             <div className="categories">
         
-                {/*<Link to='/category/novelas' className="Option">Novela</Link>
-                <Link to='/category/romance' className="Option">Romance</Link>
-                <Link  to='/category/ciencia ficcion' className="Option">Ciencia ficcion</Link>*/}
+            
                 <NavLink to='/category/novela' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Novela</NavLink>
                 <NavLink to='/category/romance' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Romance</NavLink>
                 <NavLink to='/category/ciencia ficcion' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>ciencia ficcon</NavLink>
